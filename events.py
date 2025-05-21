c1fc2526ebe111cbb5b08ebcb234b985c68c41fe
Joel Halen <andy@joelhalen.net>
2025-05-21 13:22:33 -0400

    Event system infrastructure (just laying some foundation)

---
+++ events.py
@@ -0,0 +1,33 @@
+"""
+    The DropTracker event system integrates the tracking functionality of the DropTracker
+    with the Discord bot and XenForo website to provide a fully integrated event experience
+
+"""
+
+import os
+from dotenv import load_dotenv
+import interactions
+from interactions import ActionRow, Button, ButtonStyle, ContainerComponent, MediaGalleryComponent, MediaGalleryItem, Message, PartialEmoji, SectionComponent, SeparatorComponent, TextDisplayComponent, ThumbnailComponent, UnfurledMediaItem, listen
+from interactions.api.events import MessageCreate, ComponentCompletion, ButtonPressed, Startup
+from db.models import ItemList, NpcList, Player, Session, Drop, CombatAchievementEntry, PersonalBestEntry, CollectionLogEntry
+from db.ops import get_formatted_name
+from utils.format import format_number, get_current_partition
+from utils.redis import redis_client
+
+load_dotenv()
+
+bot = interactions.Client(token=os.getenv("TEST_BOT_TOKEN"))
+
+@interactions.listen(MessageCreate)
+async def on_message_create(event: MessageCreate):
+    pass
+
+@listen(Startup)
+async def on_startup(event: Startup):
+    pass
+
+
+
+if __name__ == "__main__":
+    bot.start()
+
